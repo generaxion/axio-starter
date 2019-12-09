8b650ceadc7e8f77e005f846f840a85f74f61735
Teemu Suoranta <teemu.suoranta@aucor.fi>
2019-12-09 16:44:32 +0200

    Modify Gutenberg default settings, table block default styles

--- assets/scripts/editor-gutenberg.js
+++ assets/scripts/editor-gutenberg.js
@@ -7,21 +7,73 @@
  */
 wp.domReady(function() {
 
-  // remove button styles
-  wp.blocks.unregisterBlockStyle('core/button', 'outline');
-  wp.blocks.unregisterBlockStyle('core/button', 'squared');
+  // disable default variants
+  wp.blocks.unregisterBlockStyle('core/button',     'outline');
+  wp.blocks.unregisterBlockStyle('core/button',     'squared');
+  wp.blocks.unregisterBlockStyle('core/image',      'circle-mask');
+  wp.blocks.unregisterBlockStyle('core/quote',      'large');
+  wp.blocks.unregisterBlockStyle('core/separator',  'dots');
+  wp.blocks.unregisterBlockStyle('core/separator',  'wide');
 
-  // remove separator styles
-  wp.blocks.unregisterBlockStyle('core/separator', 'dots');
-  wp.blocks.unregisterBlockStyle('core/separator', 'wide');
-
-  // remove quote styles
-  wp.blocks.unregisterBlockStyle('core/quote', 'large');
-
-//  // add lead paragraph style
-//  wp.blocks.registerBlockStyle('core/paragraph', {
-//    name: 'lead',
-//    label: 'Lead paragraph',
-//  });
+  // add custom variants
+  //  wp.blocks.registerBlockStyle('core/paragraph', {
+  //    name: 'lead',
+  //    label: 'Lead paragraph',
+  //  });
 
 });
+
+/**
+ * Modify alignment options
+ */
+wp.hooks.addFilter(
+  'blocks.registerBlockType',
+  'aucor-starter/filters',
+  function(settings, name) {
+
+    var align;
+
+    switch(name) {
+
+      case 'core/gallery':
+      case 'core/table':
+        align = ['wide', 'full'];
+        break;
+
+      case 'core/file':
+      case 'core/quote':
+        align = false;
+        break;
+
+      case 'core/button':
+        align = ['center'];
+        break;
+
+      default:
+        return settings;
+
+    }
+
+    return lodash.assign({}, settings, {
+      supports: lodash.assign({}, settings.supports, {
+        align: align,
+      }),
+    });
+
+  }
+);
+
+/**
+ * Force default block classnames (ie. 'wp-block-list')
+ */
+wp.hooks.addFilter(
+  'blocks.registerBlockType',
+  'aucor-starter/filters',
+  function (settings, name) {
+    return lodash.assign({}, settings, {
+      supports: lodash.assign({}, settings.supports, {
+        className: true
+      }),
+    });
+  }
+);
