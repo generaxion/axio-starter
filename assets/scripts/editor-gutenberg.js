985ce172ec72eafc5fb89739f3d1c3a18e85f371
Teemu Suoranta <teemu.suoranta@aucor.fi>
2019-12-13 15:14:39 +0200

    Disable stripes style from tables

--- assets/scripts/editor-gutenberg.js
+++ assets/scripts/editor-gutenberg.js
@@ -14,6 +14,7 @@ wp.domReady(function() {
   wp.blocks.unregisterBlockStyle('core/quote',      'large');
   wp.blocks.unregisterBlockStyle('core/separator',  'dots');
   wp.blocks.unregisterBlockStyle('core/separator',  'wide');
+  wp.blocks.unregisterBlockStyle('core/table',      'stripes');
 
   // add custom variants
   //  wp.blocks.registerBlockStyle('core/paragraph', {
