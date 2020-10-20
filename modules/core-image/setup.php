b9644413a88ef9c1665a9e6bd64e4c0cb6c1049e
Teemu Suoranta <teemu.suoranta@aucor.fi>
2020-10-20 10:56:27 +0300

    Block core-image: move sizing fix to module

--- modules/core-image/setup.php
+++ modules/core-image/setup.php
@@ -14,3 +14,37 @@ add_filter('allowed_block_types', function($blocks, $post) {
   return $blocks;
 
 }, 11, 2);
+
+/**
+ * Set wide image sizes attribute
+ *
+ * Gutenberg has at the moment a bit lacking logic for sizes attributes.
+ * This fixes some issues where big images appear low-res.
+ *
+ * @see https://github.com/WordPress/gutenberg/issues/6131
+ *
+ * @param string $html the rendered markup
+ * @param array  $args the options
+ *
+ * @return string $html
+ */
+add_filter('render_block', function ($html, $args) {
+
+  if ($args['blockName'] === 'core/image' && isset($args['attrs']['align'])) {
+
+    if ($args['attrs']['align'] === 'full') {
+
+      $html = str_replace('<img ', '<img sizes="100vw" ', $html);
+
+    } elseif ($args['attrs']['align'] === 'wide') {
+
+      $base_width = (isset($GLOBALS['content_width'])) ? absint($GLOBALS['content_width']) : 720;
+      $wide_width = absint(1.5 * $base_width);
+      $html = str_replace('<img ', '<img sizes="(min-width: ' . $wide_width . 'px) ' . $wide_width . 'px, 100vw" ', $html);
+
+    }
+
+  }
+  return $html;
+
+}, 10, 2);
