9d0bfcd7e8e55600c5892f8c2ceb7be73d4fe4f7
Samuli Martikainen <samuli.martikainen@generaxion.fi>
2021-10-12 09:00:36 +0300

    add: cookiebot module with fix for problem where CookieBot is blocking main.js when only necessary cookies are allowed

---
+++ modules/cookiebot/setup.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Cookiebot
+ *
+ * @package axio
+ */
+
+/*
+ * Add attribute data-cookieconsent="ignore" to x-js script
+ * 
+ * Fixes problem where CookieBot is blocking main.js when only necessary cookies are allowed
+ */
+add_filter('script_loader_tag', function($tag, $handle, $src) {
+  if (in_array($handle, ['x-js'])) {
+    $tag = str_replace('<script ' , '<script data-cookieconsent="ignore" ', $tag);
+  }
+  return $tag;
+}, 10, 3);
